3d2696cff5c3645c7832d6a5ddae17d1632b7841
Will Knez <wbknez.dev@gmail.com>
2017-11-21 17:37:10 -0700

    Add core initialization file to Emacs.

---
+++ emacs/emacs.d/init.el
@@ -0,0 +1,30 @@
+;;; -*- mode: Emacs-Lisp -*-
+;;
+;;; Summary: Initializes all relevant user configuration options in Emacs.
+
+;; Refrain from loading outdated byte code.
+(setq load-prefer-newer t)
+
+;; Added by Package.el.
+;(package-initialize)
+
+;; Load paths to custom script location(s).
+;;
+;; Custom scripts are in ~/.emacs.d/lisp.
+;; Custom language and UI scripts are in ~/.emacs.d/lisp/extra
+;; Theme scripts are in ~/.emacs.d/themes
+(add-to-list 'load-path
+             (expand-file-name "lisp" user-emacs-directory))
+(add-to-list 'load-path
+	     (expand-file-name "lisp/extra" user-emacs-directory))
+
+;; Load utility script(s).
+(require 'bootstrap-use-package)
+
+;; Load other customizations.
+(require 'coding-customize)
+(require 'editing-customize)
+
+;; Load custom variables.
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file)
