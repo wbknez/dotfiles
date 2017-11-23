7ef44b489d3a0a90642b99705465b70cec96e727
Will Knez <wbknez.dev@gmail.com>
2017-11-22 18:19:06 -0700

    Add autoload function configuration and aliases.

---
+++ zsh/zsh.d/shell/extra/functions.zsh
@@ -0,0 +1,10 @@
+# -*- mode: shell-script[zsh] -*-
+#
+# Summary: Load and alias custom functions from FPATH.
+
+autoload -Uz extract-file
+autoload -Uz maven-jar-install
+autoload -Uz start-urxvt-daemon
+
+alias exf='extract-file'
+alias mvn-ji='maven-jar-install'
