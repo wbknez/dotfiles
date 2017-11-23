f139eeb0b553205ac1e37158a8b463f2da0822b4
Will Knez <wbknez.dev@gmail.com>
2017-11-23 14:17:04 -0700

    Add Python aliases to ZSH.

---
+++ zsh/zsh.d/shell/extra/python.zsh
@@ -0,0 +1,13 @@
+# -*- mode: shell-script[zsh] -*
+#
+# Summary: Adds Python-specific aliases to ZSH.
+
+alias deenv='source deactivate'
+alias mkenv='python3 -m venv'
+alias no2='python3 -m nose2'
+alias pp3='pip3'
+alias pp3g='pip freeze | grep -i'
+alias pp3i='pip3 install --user'
+alias pp3r='pip3 remove'
+alias py3='python3'
+alias py3srv='python3 -m http.server'
