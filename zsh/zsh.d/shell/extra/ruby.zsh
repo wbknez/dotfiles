5a8c613ff596e98edffff1ece7a97fc0f1ed61f9
Will Knez <wbknez.dev@gmail.com>
2017-11-27 12:58:01 -0700

    Add functions to update rbenv.

--- zsh/zsh.d/shell/extra/ruby.zsh
+++ zsh/zsh.d/shell/extra/ruby.zsh
@@ -2,13 +2,56 @@
 #
 # Summary: Add Ruby-specific aliases and capabilities to ZSH via rbenv.
 
-if [[ ! -z "$RB_ENV_HOME" && -d "$RB_ENV_HOME" ]]; then
-    path=($RB_ENV_HOME/bin $path)
-    eval "$(rbenv init -)"
-else
-    warn-msg "(ruby) Could not find RB_ENV_HOME."
-    warn-msg "(ruby) Failed to initialize Ruby configuration for ZSH."
-fi
+# Loads rbenv by sourcing its initialization file.
+#
+# This function may also be called to reload rbenv in a current shell.
+function rbenv-load() {
+    if [[ ! -z $"RB_ENV_HOME" && -d "$RB_ENV_HOME" ]]; then
+        path=($RB_ENV_HOME/bin $path)
+        eval "$(rbenv init -)"
+    else
+        error-msg "rbenv is not installed."
+    fi
+}
+
+# Checks to see if the current rbenv installation is out of date and, if so,
+# pulls the latest changes from upstream.
+function rbenv-update() {
+    if [[ ! -d "$RB_ENV_HOME" ]]; then
+        error-msg "rbenv is not installed."
+        exit
+    fi
+
+    local RUBY_BUILD_DIR="$RB_ENV_HOME/plugins/ruby-build"
+    
+    if [[ -d "$RUBY_BUILD_DIR" ]]; then
+            local rbuild_installed=$(cd "$RUBY_BUILD_DIR" && git describe --tags)
+            local rbuild_upstream=$(cd "$RUBY_BUILD_DIR" && git fetch --quiet origin && git describe --abbrev=0 --tags --match "v[0-9]*" origin)
+
+            if [[ "$rbuild_installed" != "$rbuild_upstream" ]]; then
+                echo "Updating ruby-build to version $rbuild_upstream..."
+                $(cd "$RUBY_BUILD_DIR" && git fetch --quiet && git checkout "$rbuild_upstream")
+                confirm-msg "ruby-build updated successfully."
+            else
+                confirm-msg "ruby-build $rbuild_installed is up to date."
+            fi
+    fi
+    
+    local installed_version=$(cd "$RB_ENV_HOME" && git describe --tags)
+    local upstream_version=$(cd "$RB_ENV_HOME" && git fetch --quiet origin && git describe --abbrev=0 --tags --match "v[0-9]*" origin)
+    
+    if [[ "$installed_version" != "$upstream_version" ]]; then
+        echo "Updating rbenv to version $upstream_version..."
+        $(cd "$RB_ENV_HOME" && git fetch --quiet && git checkout "$upstream_version")
+        echo "Reloading rbenv..."
+        rbenv-load
+        confirm-msg "rbenv updated successfully."
+    else
+        confirm-msg "rbenv $installed_version is up to date."
+    fi
+}
+
+rbenv-load
 
 alias rbinst='rbenv install'
 alias rblst='rbenv install --list'
