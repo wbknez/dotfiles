c9abd048e4c7e2e3fac4914031084c14af59c2ba
Will Knez <wbknez.dev@gmail.com>
2017-11-28 21:52:40 -0700

    Update Markdown configuration to use use-package nested syntax.

--- emacs/emacs.d/lisp/extra/markdown-extra.el
+++ emacs/emacs.d/lisp/extra/markdown-extra.el
@@ -2,6 +2,18 @@
 ;;
 ;;; Summary: Add support for Markdown to Emacs.
 
-(use-package markdown-mode :ensure t)
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :ensure t
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("Readme\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  
+  :init
+  (setq
+   markdown-command "pandoc"   ;;
+   markdown-enable-math t      ;;
+  ))
 
 (provide 'markdown-extra)
