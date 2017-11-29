00316dd7fdc5e2411ca0b7a1019e429ae1c62f58
Will Knez <wbknez.dev@gmail.com>
2017-11-28 21:52:16 -0700

    Update Groovy configuration to use use-package nested syntax.

--- emacs/emacs.d/lisp/extra/groovy-extra.el
+++ emacs/emacs.d/lisp/extra/groovy-extra.el
@@ -3,7 +3,7 @@
 ;;; Summary: Add Groovy support to Emacs and associate Gradle build files with
 ;;; it as well.
 
-(use-package groovy-mode :ensure t :defer nil)
-(add-to-list 'auto-mode-alist '("\\.gradle\\'" . groovy-mode))
+(use-package groovy-mode :ensure t :defer nil
+  :mode ("\\.gradle\\'" . groovy-mode))
 
 (provide 'groovy-extra)
