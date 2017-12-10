348f54d75ea0e7c0f9be733b1fd5e0d23c8e764a
Will Knez <wbknez.dev@gmail.com>
2017-12-10 09:00:26 -0700

    Add mode specification for Toml-mode.

--- emacs/emacs.d/lisp/lang/toml-lang.el
+++ emacs/emacs.d/lisp/lang/toml-lang.el
@@ -2,6 +2,8 @@
 ;;
 ;;; Summary: Add support for TOML files to Emacs.
 
-(use-package toml-mode :ensure t)
+(use-package toml-mode
+  :ensure t
+  :mode ("\\.tom\\'" . toml-mode))
 
 (provide 'toml-lang)
