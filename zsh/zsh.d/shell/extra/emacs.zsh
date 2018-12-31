8059205b7be627c6818d803d402c5a9a17ec3d21
Will Knez <wbknez.dev@gmail.com>
2018-12-31 14:58:16 -0700

    Remove Emacs and Ruby configurations for ZSH.

--- zsh/zsh.d/shell/extra/emacs.zsh
+++
@@ -1,9 +0,0 @@
-# -*- mode: shell-scripts[zsh] -*-
-#
-# Summary: Adds Emacs-specific aliases and functions to ZSH.
-
-alias emd='emacs --daemon'
-alias emdsch='ps -ef | grep -i 'emacs --daemon''
-alias em='emacsclient -t'
-alias emx='emacsclient -c -a /usr/bin/emacs'
-alias sem='sudo emacsclient -t'
