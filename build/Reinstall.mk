b39fe8c65c8c5660710277829258ce6148dfa60c
Will Knez <wbknez.devgmail.com>
2019-12-22 23:00:51 -0700

    Add reinstall functionality to build script(s).

---
+++ build/Reinstall.mk
@@ -0,0 +1,14 @@
+.POSIX:
+
+.PHONY: reinstall-fish
+reinstall-git: clean-fish install-fish
+
+.PHONY: reinstall-git
+reinstall-git: clean-git install-git
+
+.PHONY: reinstall-nvim
+reinstall-nvim: clean-nvim install-nvim
+
+.PHONY: reinstall-ssh
+reinstall-ssh: clean-ssh install-ssh
+
