f6a8e9777ba095c1552d542cdc8dbf76fcdf8bdc
Will Knez <wbknez.devgmail.com>
2019-12-26 15:44:50 -0700

    Add directory functions.

---
+++ fish/config.fish
@@ -0,0 +1,8 @@
+# Summary: Configures Fish shell.
+
+# Directory manipulation.
+function ..;    cd ..; end          # Up a single level.
+function ...;   cd ../..; end       # Up two levels.
+function ....;  cd ../../..; end    # Up three levels.
+function .....; cd ../../../..; end # Up four levels.
+
