1f808fe461d40c42d34c94a7f9dead78690fffdc
Will Knez <wbknez.devgmail.com>
2019-12-26 16:48:01 -0700

    Add additional environment variables.

--- fish/conf.d/environment.fish
+++ fish/conf.d/environment.fish
@@ -9,7 +9,6 @@ set -g -x XDG_DATA_HOME "$HOME/.local/share"
 set -g -x BROWSER "/usr/bin/firefox"
 set -g -x EDITOR "/usr/bin/nvim"
 set -g -x PAGER "/usr/bin/less"
-set -g -x PIP_DOWNLOAD_CACHE "$XDG_CACHE_HOME/pip"
 set -g -x VISUAL $EDITOR
 
 # User directories.
@@ -18,9 +17,14 @@ set -g -x BIN_DIR "$HOME/bin"
 
 # User applications.
 set -g -x CONDA_HOME "$APP_DIR/Conda"
+set -g -x GIMP2_DIRECTORY="$XDG_CONFIG_HOME/gimp"
+set -g -x GNUPGHOME "$XDG_CONFIG_HOME/gnupg"
 set -g -x GRADLE_HOME "$APP_DIR/Gradle"
 set -g -x M2_HOME "$APP_DIR/Maven"
 set -g -x M2 "$M2_HOME/bin"
+set -g -x PIP_DOWNLOAD_CACHE "$XDG_CACHE_HOME/pip"
+set -g -x RANDFILE "$XDG_RUNTIME_DIR/rnd"
+set -g -x TMUX_TEMPDIR "$XDG_RUNTIME_DIR/tmux"
 
 # Path.
 set -g fish_user_paths $BIN_DIR $CONDA_HOME/bin $GRADLE_HOME/bin $M2 $fish_user_paths
