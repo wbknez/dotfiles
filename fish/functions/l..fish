ad064c42dd92f8cced1b2159b87227e4404ab761
Will Knez <wbknez.devgmail.com>
2019-12-26 19:46:04 -0700

    Fix function to display hidden files in terminal.

--- fish/functions/l..fish
+++ fish/functions/l..fish
@@ -1,11 +1,27 @@
-# Summary: Show hidden files in a directories.
+# Summary: Show hidden files in a single directory.
 
-function l. -d "View hidden files." --wrap "ls"
-    set -l target $argv/.?*
+function l. -d "View hidden files in a directory." --wrap "ls"
+    set -l argc (count $argv)
 
-    if test -z $argv
-        set target .?*
+    set -l options $argv
+    set -l target .?*
+
+    if test $argc -gt 1
+        set options $argv
+        set target
+
+        for i in (seq 1 $argc)
+            if test -d $argv[$i]
+                set target $argv[$i]/.?*
+                set --erase options[$i]
+
+                break
+            end
+        end
+    else if test $argc -eq 1; and test -d $argv
+        set options
+        set target $argv/.?*
     end
 
-    ls -d $target
+    ls -d $options $target
 end
